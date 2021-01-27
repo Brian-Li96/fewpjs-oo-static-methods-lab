ee8828b9b4cc2c2c0efc4e5e789c24f258cbe688
Brian Li <Brian.Li96@yahoo.com>
2021-01-27 11:42:29 -0600

    commit tree dd1f39329deaa5fdc6f519eb526844f92da5b4b9

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '');
+  }
+
+  static titleize(string) {
+    const avoid = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let newString = []
+    let array = string.split(" ")
+    for (let i = 0; i < array.length; i++) {
+      if (i == 0) {
+        newString.push(this.capitalize(array[i]))
+      } else {
+        if (avoid.includes(array[i])) {
+          newString.push(array[i])
+        } else {
+          newString.push(this.capitalize(array[i]))
+        }
+      }
+    }
+    return newString.join(' ')
+  }
 }
